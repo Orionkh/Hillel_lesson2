fa7f246a70df0391faee71e939f545147d5cc76d
macbook <macbook@macbooks-MacBook-Pro.local>
2023-05-14 23:43:37 +0300

    first commit

---
+++ instructions.py
@@ -0,0 +1,31 @@
+a = 1
+b = 2
+c = 3
+
+if (a + b) == c:
+    print('true1')
+else:
+    print('false1')
+
+if a > b:
+    print('true2')
+else:
+    print('false2')
+
+if b != 0:
+    print(c / b)
+
+if a == b:
+    print('true3')
+
+if a != b:
+    print('true4')
+
+
+if a > b:
+    print('false5')
+elif a > c:
+    print('false55')
+else:
+    print('true5')
+
---
+++ main.py
@@ -0,0 +1,33 @@
+element = 1
+same = 2
+Same = 3
+element_four = 4
+element5 = 5
+_element6 = 6
+ELEMENT7 = 7
+creative = element + element5 - same
+
+print(element + same)
+print(ELEMENT7 - Same)
+print(_element6 / Same)
+print(same * Same)
+print(Same ** element5)
+print(ELEMENT7 // Same)
+print(ELEMENT7 % Same)
+print((same * Same) + (element5 - element))
+print('\n')
+
+element += 34
+print(element)
+same -= 3
+print(same)
+Same /= 3
+print(Same)
+element_four *= 3
+print(element_four)
+element5 **=2
+print(element5)
+_element6 //= 0.5
+print(_element6)
+creative %= 3
+print(creative)
